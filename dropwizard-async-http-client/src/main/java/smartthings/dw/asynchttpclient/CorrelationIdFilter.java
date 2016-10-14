6b7ff97f45a3bdd165fce16d60ee6328457dfd3a
dylanbijnagte <Dylan.Bijnagte@smartthings.com>
2016-10-14 09:26:42 -0500

    copy MDC to thread where async handler executes

--- dropwizard-async-http-client/src/main/java/smartthings/dw/asynchttpclient/CorrelationIdFilter.java
+++ dropwizard-async-http-client/src/main/java/smartthings/dw/asynchttpclient/CorrelationIdFilter.java
@@ -1,18 +1,87 @@
 package smartthings.dw.asynchttpclient;
 
+import io.netty.handler.codec.http.HttpHeaders;
+import org.asynchttpclient.*;
 import org.asynchttpclient.filter.FilterContext;
 import org.asynchttpclient.filter.FilterException;
 import org.asynchttpclient.filter.RequestFilter;
+import org.slf4j.MDC;
 import smartthings.dw.logging.LoggingContext;
+
+import java.util.Map;
 
 public class CorrelationIdFilter implements RequestFilter {
 
 	@Override
 	public <T> FilterContext<T> filter(FilterContext<T> ctx) throws FilterException {
+
+		final Map<String, String> mdc = MDC.getCopyOfContextMap();
+		final AsyncHandler<T> asyncHandler = ctx.getAsyncHandler();
+
+		// Wrap AsyncHandler to copy MDC since it executes on a different thread
+		FilterContext<T> newContext = new FilterContext.FilterContextBuilder<>(ctx)
+				.asyncHandler(new AsyncHandler<T>() {
+					@Override
+					public void onThrowable(Throwable t) {
+						try {
+							mdc.forEach(MDC::put);
+							asyncHandler.onThrowable(t);
+						} finally {
+							MDC.clear();
+						}
+					}
+
+					@Override
+					public State onBodyPartReceived(HttpResponseBodyPart bodyPart) throws Exception {
+						State result;
+						try {
+							mdc.forEach(MDC::put);
+							result = asyncHandler.onBodyPartReceived(bodyPart);
+						} finally {
+							MDC.clear();
+						}
+						return result;
+					}
+
+					@Override
+					public State onStatusReceived(HttpResponseStatus responseStatus) throws Exception {
+						try {
+							mdc.forEach(MDC::put);
+							return asyncHandler.onStatusReceived(responseStatus);
+						} finally {
+							MDC.clear();
+						}
+					}
+
+					@Override
+					public State onHeadersReceived(HttpResponseHeaders headers) throws Exception {
+						try {
+							mdc.forEach(MDC::put);
+							return asyncHandler.onHeadersReceived(headers);
+						} finally {
+							MDC.clear();
+						}
+					}
+
+					@Override
+					public T onCompleted() throws Exception {
+						try {
+							mdc.forEach(MDC::put);
+							return asyncHandler.onCompleted();
+						} finally {
+							MDC.clear();
+						}
+					}
+				}).build();
+
 		String loggingId = LoggingContext.getLoggingId();
+
 		if (loggingId != null) {
-			ctx.getRequest().getHeaders().add(LoggingContext.CORRELATION_ID_HEADER, loggingId);
+			HttpHeaders headers = newContext.getRequest().getHeaders();
+			if (!headers.contains(LoggingContext.CORRELATION_ID_HEADER)) {
+				headers.add(LoggingContext.CORRELATION_ID_HEADER, loggingId);
+			}
 		}
-		return ctx;
+		return newContext;
 	}
 }
---
+++ dropwizard-async-http-client/src/test/groovy/smartthings/dw/asynchttpclient/CorrelationIdFilterSpec.groovy
@@ -0,0 +1,148 @@
+package smartthings.dw.asynchttpclient
+
+import io.netty.handler.codec.http.HttpHeaders
+import org.asynchttpclient.AsyncHandler
+import org.asynchttpclient.HttpResponseBodyPart
+import org.asynchttpclient.HttpResponseHeaders
+import org.asynchttpclient.HttpResponseStatus
+import org.asynchttpclient.Request
+import org.asynchttpclient.filter.FilterContext
+import org.slf4j.MDC
+import smartthings.dw.logging.LoggingContext
+import spock.lang.Specification
+
+class CorrelationIdFilterSpec extends Specification {
+
+	CorrelationIdFilter correlationIdFilter = new CorrelationIdFilter()
+	Request request = Mock()
+
+	HttpHeaders requestHeaders = Mock()
+	static final String FOO = 'foo'
+
+	def setup() {
+		MDC.put(FOO, 'bar')
+		MDC.put('loggingId', 'log-id')
+		0 * _
+	}
+
+	def cleanup() {
+		MDC.clear()
+	}
+
+	def 'wrapped async handler adds and removes MDC'() {
+		given:
+		def callResults = [:]
+
+		def ctx = new FilterContext.FilterContextBuilder().asyncHandler(new AsyncHandler() {
+			@Override
+			void onThrowable(Throwable t) {
+				callResults.throwable = MDC.get(FOO)
+			}
+
+			@Override
+			AsyncHandler.State onBodyPartReceived(HttpResponseBodyPart bodyPart) throws Exception {
+				callResults.onBodyPart = MDC.get(FOO)
+				return AsyncHandler.State.CONTINUE
+			}
+
+			@Override
+			AsyncHandler.State onStatusReceived(HttpResponseStatus responseStatus) throws Exception {
+				callResults.onStatus = MDC.get(FOO)
+				return AsyncHandler.State.ABORT
+			}
+
+			@Override
+			AsyncHandler.State onHeadersReceived(HttpResponseHeaders headers) throws Exception {
+				callResults.onHeaders = MDC.get(FOO)
+				return AsyncHandler.State.UPGRADE
+			}
+
+			@Override
+			Object onCompleted() throws Exception {
+				callResults.onComplete = MDC.get(FOO)
+				return 'result'
+			}
+		}).request(request).build()
+
+		when:
+		def updated = correlationIdFilter.filter(ctx)
+
+		then:
+		!updated.is(ctx)
+		_ * request.headers >> requestHeaders
+		1 * requestHeaders.contains(LoggingContext.CORRELATION_ID_HEADER) >> false
+		1 * requestHeaders.add(LoggingContext.CORRELATION_ID_HEADER, 'log-id')
+
+		when:
+		String fooBefore
+		String fooAfter
+		Thread.start {
+			MDC.clear()
+			fooBefore = MDC.get(FOO)
+			updated.asyncHandler.onThrowable(new Exception())
+			fooAfter = MDC.get(FOO)
+		}.join()
+
+		then:
+		callResults.throwable == 'bar'
+		fooBefore == null
+		fooAfter == null
+
+		when:
+		def result
+		Thread.start {
+			MDC.clear()
+			fooBefore = MDC.get(FOO)
+			result = updated.asyncHandler.onBodyPartReceived(null)
+			fooAfter = MDC.get(FOO)
+		}.join()
+
+		then:
+		callResults.onBodyPart == 'bar'
+		fooBefore == null
+		fooAfter == null
+		result == AsyncHandler.State.CONTINUE
+
+		when:
+		Thread.start {
+			MDC.clear()
+			fooBefore = MDC.get(FOO)
+			result = updated.asyncHandler.onStatusReceived(null)
+			fooAfter = MDC.get(FOO)
+		}.join()
+
+		then:
+		callResults.onStatus == 'bar'
+		fooBefore == null
+		fooAfter == null
+		result == AsyncHandler.State.ABORT
+
+		when:
+		Thread.start {
+			MDC.clear()
+			fooBefore = MDC.get(FOO)
+			result = updated.asyncHandler.onHeadersReceived(null)
+			fooAfter = MDC.get(FOO)
+		}.join()
+
+		then:
+		callResults.onHeaders == 'bar'
+		fooBefore == null
+		fooAfter == null
+		result == AsyncHandler.State.UPGRADE
+
+		when:
+		Thread.start {
+			MDC.clear()
+			fooBefore = MDC.get(FOO)
+			result = updated.asyncHandler.onCompleted()
+			fooAfter = MDC.get(FOO)
+		}.join()
+
+		then:
+		callResults.onComplete == 'bar'
+		fooBefore == null
+		fooAfter == null
+		result == 'result'
+	}
+}
